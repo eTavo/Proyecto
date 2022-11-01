bd39990fb3b3461c03a7f5e8ceba9a941ccdea1b
Emma <emmanueltavolaro@gmail.com>
2022-10-31 22:05:36 -0300

    variables de entorno

--- app.js
+++ app.js
@@ -2,8 +2,21 @@
 const express = require('express');
 const app = express();
 
+require('dotenv').config()
+
 // se define un puerto 
 const port = process.env.PORT || 3000;
+
+//conexion a base de datos
+const mongoose = require('mongoose');
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.iyn5zgg.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
+
+mongoose.connect(uri,
+    {useNewUrlParser: true, useUnifiedTopology: true}
+)   
+    .then(() => console.log('Base de datos conectada'))
+    .catch(e => console.log(e))
 
 //motor de plantillas
 app.set('view engine', 'ejs');
@@ -11,23 +24,21 @@ app.set('views', __dirname + '/views');
 
 app.use(express.static(__dirname + "/public"));
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
+//rutas web
+app.use('/', require('./router/RutasWeb'));
+app.use('/mascotas', require('./router/Mascotas'));
 
-app.get('/home', (req, res) => {
-    res.render("home")
-})
-
-app.get('/programs', (req, res) => {
-    res.render("programs")
-})
-
-app.get('/calculadora', (req, res) => {
-    res.render("calculadora")
+app.use((req,res,next) => {
+    res.status(404).render("404", {
+        titulo: "404",
+        descripcion: "Este enlace no se encuentra o no existe."
+    })
 })
 
 app.listen(port, () => {
     console.log('Servidor a su servicio en puerto:', port)
 })
 
+// https://www.youtube.com/watch?v=IpQSsb-1N1g&list=PLPl81lqbj-4IEnmCXEJeEXPepr8gWtsl6&index=14   10:12 min
+
+// https://nodejs-emmanuel.herokuapp.com/  ----> url de pagina en la nube
---
+++ router/Mascotas.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+const Mascota = require('../models/mascota')
+
+router.get('/', async (req,res) => {
+
+    try{
+
+        const arrayMascotasDB = await Mascota.find()
+        console.log(arrayMascotasDB)
+
+        res.render('mascotas', {
+
+            arrayMascotas : arrayMascotasDB
+
+            //arrayMascotas: [
+            //    {id: 1, nombre: 'Perro', descripcion: 'Henry'},
+            //    {id: 2, nombre: 'Hamster', descripcion: 'Sin nombre'},
+            //]
+        })
+
+    }catch (error){
+        console.log(error)
+    }
+})
+
+module.exports = router;
