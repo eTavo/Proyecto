d9300d27304f632f64a0ed644b9446880867920a
Emma <emmanueltavolaro@gmail.com>
2022-10-31 23:32:57 -0300

    conexion a mongodb

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const mongoose = require('mongoose');
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.iyn5zgg.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
 
 mongoose.connect(uri,
-    {useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }
+    {useNewUrlParser: true, useUnifiedTopology: true}
 )   
     .then(() => console.log('Base de datos conectada'))
     .catch(e => console.log(e))
