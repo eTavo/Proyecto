40f2de58391f22e595330f75531367caa0325bd5
Emma <emmanueltavolaro@gmail.com>
2022-11-09 03:19:26 -0300

    nuevo programs

--- router/Mascotas.js
+++ router/Mascotas.js
@@ -51,4 +51,51 @@ router.post('/', async(req, res) => {
     }
 })
 
+router.get('/:id', async (req,res) => {
+    
+    const id = req.params.id
+
+    try {
+        
+        const mascotaDB = await Mascota.findOne({_id: id})
+        console.log(mascotaDB)
+
+        res.render('detalle', {
+            mascota : mascotaDB,
+            error: false
+        })
+    } catch (error) {
+
+        console.log(error)
+
+        res.render('detalle', {
+            error: true,
+            mensaje: 'No se encuentra el id seleccionado'
+        })
+    }
+})
+
+router.delete('/:id', async (req,res) => {
+    
+    const id = req.params.id
+    try {
+        const mascotaDB = await Mascota.findByIdAndDelete({_id: id})
+
+        if (mascotaDB) {
+            res.json({
+                estado: true,
+                mensaje: 'eliminado!'
+            })
+        }else{
+            res.json({
+                estado: false,
+                mensaje: 'fallo eliminar!'
+            })
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
 module.exports = router;
